b6a4253a65a24f4f5004dabe545c5f63cfdbf7ed
Anthony Tamayo Ortega <anthony.tamayo@dreamcodesoft.com>
2021-04-25 12:48:47 -0500

    the consulting is done at home

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Inscription } from 'src/app/interfaces/inscription.interface';
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,7 @@ export class RegisterComponent implements OnInit {
   public descriptionRegister: string;
   public textBtnNext: string;
   public textBtnPrevious: string;
+  public company: Inscription;
 
   constructor(private router: Router) {
     this.titleRegister =
@@ -19,10 +21,13 @@ export class RegisterComponent implements OnInit {
       'La empresa se encuentra registrada en la Camara de Comercio de Medellín para Antioquia. Para acceder al servicio de Trámites Virtuales se utilizarán los datos reportados en el registro. <br> Los campos marcados con asterisco (*) son obligatorios.';
     this.textBtnNext = 'Continuar >';
     this.textBtnPrevious = '< Regresar';
+
+    console.log('receive params', this.router.getCurrentNavigation().extras.queryParams);
+    this.company = this.router.getCurrentNavigation().extras.queryParams.data;
   }
 
   ngOnInit(): void {}
-  saveCompany() {}
+  updateCompany() {}
   goBack() {
     this.router.navigate(['']);
   }
