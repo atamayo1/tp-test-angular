dae47d6833d051b496acb33e190e243aefcdd45e
Anthony Tamayo Ortega <anthony.tamayo@dreamcodesoft.com>
2021-04-25 22:30:48 -0500

    i add changes on environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   domain: 'http://localhost:3000/',
 };
 
